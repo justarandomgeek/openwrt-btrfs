ef318c8c25fa6a01ff9073b732096e2ba284a6d7
justarandomgeek <justarandomgeek@gmail.com>
2023-04-09 21:16:23 -0400

    btrfs subtarget

---
+++ target/linux/x86/btrfs/target.mk
@@ -0,0 +1,6 @@
+ARCH:=x86_64
+BOARDNAME:=x86_64_Btrfs
+
+define Target/Description
+        Build images for 64 bit systems including virtualized guests, using btrfs rootfs for upgrade slots.
+endef
