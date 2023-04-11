6548515c54b7d77b2f295a80011cef197ab866be
justarandomgeek <justarandomgeek@gmail.com>
2023-04-11 11:41:19 -0400

    subtarget is worse

--- merge_rootfs_with_kernel.sh
+++ merge_rootfs_with_kernel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cp bin/targets/x86/btrfs/openwrt-x86-btrfs-generic-kernel.bin build_dir/target-x86_64_musl/root-x86/vmlinuz
-tar -cp --numeric-owner --owner=0 --group=0 --mode=a-s --sort=name -C build_dir/target-x86_64_musl/root-x86/ . | gzip -9n > bin/targets/x86/btrfs/openwrt-x86-btrfs-generic-rootfs-kernel.tar.gz
+cp bin/targets/x86/64/openwrt-x86-64-generic-kernel.bin build_dir/target-x86_64_musl/root-x86/vmlinuz
+tar -cp --numeric-owner --owner=0 --group=0 --mode=a-s --sort=name -C build_dir/target-x86_64_musl/root-x86/ . | gzip -9n > bin/targets/x86/64/openwrt-x86-64-generic-rootfs-kernel.tar.gz
--- target/linux/x86/btrfs/target.mk
+++
@@ -1,6 +0,0 @@
-ARCH:=x86_64
-BOARDNAME:=x86_64_Btrfs
-
-define Target/Description
-        Build images for 64 bit systems including virtualized guests, using btrfs rootfs for upgrade slots.
-endef
