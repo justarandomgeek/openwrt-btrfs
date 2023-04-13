cbb254995eee7b131b22fd82aebedcc5d46d7c85
justarandomgeek <justarandomgeek@gmail.com>
2023-04-13 13:24:03 -0400

    merge script

---
+++ make_btrfs_images.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp bin/targets/x86/64/openwrt-x86-64-generic-kernel.bin build_dir/target-x86_64_musl/root-x86/vmlinuz
+tar -cp --numeric-owner --owner=0 --group=0 --mode=a-s --sort=name -C build_dir/target-x86_64_musl/root-x86/ . | gzip -9n > bin/targets/x86/64/openwrt-x86-64-generic-rootfs-kernel.tar.gz
+
+cp staging_dir/target-x86_64_musl/image/grub2/bootx64.efi bin/targets/x86/64/
